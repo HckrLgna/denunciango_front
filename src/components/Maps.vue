e62311ec0fee3c7b31d0bfcad6c049d4a62a83eb
Angel Eugenio Charca Blanco <ablanco837@gmail.com>
2023-06-15 17:34:08 -0400

    mejoramos la presentacion

--- src/components/Maps.vue
+++ src/components/Maps.vue
@@ -1,71 +1,163 @@
 <template>
-  <div class="p-5">
-    <b-container>
-        <form  >
-          <b-row md="8" class="p-3">
-            <b-col md="3">
-              <b-form-group
-              id="input-group-estado"
-              label="Seleccione un estado:"
-              label-for="input-estado">
-                <b-form-select
-                  id="input-estado"
-                  v-model="estado"
-                  :options="estados"
-                  size="sm"
-                  @change="handleEstadoChange"
-                ></b-form-select>
-              </b-form-group>
-              
-            </b-col>  
-            <b-col md="3">
-              <b-form-group
-              id="input-group-tipo"
-              label="Seleccione un tipo:"
-              label-for="input-tipo"
-              >
-                <b-form-select
-                  id="input-tipo"
-                  v-model="tipo"
-                  :options="tipos"
-                  @change="handleTipoChange"
-                ></b-form-select>
-              </b-form-group>
-              
-            </b-col>
-            <b-col md="3">
-              <b-form-datepicker
-                v-model="filterFechaInicio"
-                placeholder="Fecha de inicio"
-                @change="handleFechaInicioChange"
-              ></b-form-datepicker>
-            </b-col>
-            <b-col md="3">
-              <b-form-datepicker
-                v-model="filterFechaFin"
-                placeholder="Fecha de fin"
-                @change="handleFechaFinChange"
-              ></b-form-datepicker>
-            </b-col>
-           
-          </b-row>
-          <b-row md="12">
-              <b-form-checkbox
-                id="checkbox-1"
-                v-model="status"
-                name="checkbox-1"
-                value="accepted"
-                unchecked-value="not_accepted"
-                @change="handleStatusChange"
-              >
-                Aplicar filtros
-              </b-form-checkbox>
-            </b-row>
+  <div  >
+    <b-container fluid class="bg-white p-5 ">
+      <b-row class="px-5 py-4">
+        <h1>Categorias de denuncias</h1>
+      </b-row>
+      <b-row class="px-4">
  
-        </form>
+          <b-col>
+            <b-card
+              bg-variant="success" text-variant="white" class="text-center"
+              title="Area verde"
+              img-src="https://picsum.photos/600/300/?image=25"
+              img-alt="Image"
+              img-top
+              tag="article"
+              style="max-width: 20rem;"
+            >
+              <b-card-text>
+                "Área Verde" se refiere a la comunicación o reporte que se realiza para informar sobre una situación relacionada con un área o espacio destinado a la vegetación y la naturaleza.
+              </b-card-text>
 
-      
-      <div ref="googleMap" class="google-map"></div>
+            </b-card>
+          </b-col>
+  
+        
+        <b-col>
+          <b-card
+            bg-variant="secondary" text-variant="white" class="text-center"
+            title=" Via publica"
+            img-src="https://picsum.photos/600/300/?image=25"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 20rem;"
+          >
+            <b-card-text>
+              "Vía Pública" se refiere a cualquier reporte o queja relacionada con problemas, irregularidades o situaciones que afectan la calidad, el orden o la seguridad de las vías públicas de una determinada localidad. 
+            </b-card-text>
+
+          </b-card>
+        </b-col>
+        <b-col>
+          <b-card
+            bg-variant="danger" text-variant="white"   class="text-center"
+            title="Servicio de Basura"
+            img-src="https://picsum.photos/600/300/?image=25"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 20rem;"
+          >
+            <b-card-text>
+               "Servicio de Basura" se refiere a situaciones relacionadas con la gestión inadecuada o deficiente del servicio de recolección y manejo de residuos sólidos en una determinada área o comunidad.
+            </b-card-text>
+
+          </b-card>
+        </b-col>
+        <b-col>
+          <b-card 
+            bg-variant="info" text-variant="white"  class="text-center"
+            title="Alumbrado"
+            img-src="https://picsum.photos/600/300/?image=25"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 20rem;"
+          >
+            <b-card-text>
+              "Alumbrado" se refiere a un reporte o queja relacionada con el sistema de iluminación de una determinada área, como calles, parques, aceras u otros espacios públicos.  
+            </b-card-text>
+
+          </b-card>
+        </b-col>
+
+      </b-row>
+ 
+    </b-container>
+
+    <b-container fluid class="bg-light p-5 ">
+        <b-row class="px-5 py-2 mx-3">
+          <h2>Visualiza las denuncias cercanas</h2>
+        </b-row>
+        <b-row class="px-5 py-4 align-items-center">
+        <b-col class="col-4 p-1 h-100">
+          <b-form-group
+            id="input-group-estado"
+            label="Seleccione un estado:"
+            label-for="input-estado"
+          >
+            <b-form-select
+              id="input-estado"
+              v-model="estado"
+              :options="estados"
+              @change="handleEstadoChange"
+            ></b-form-select>
+          </b-form-group>
+        </b-col>
+        <b-col class="col-4 p-1 h-100">
+          <b-form-group
+            id="input-group-tipo"
+            label="Seleccione un tipo:"
+            label-for="input-tipo"
+          >
+            <b-form-select
+              id="input-tipo"
+              v-model="tipo"
+              :options="tipos"
+              @change="handleTipoChange"
+            ></b-form-select>
+          </b-form-group>
+        </b-col>
+        <b-col class="col-6 p-1 h-100">
+          <b-form-group
+            id="input-group-fechaIni"
+            label="Seleccione una fecha inicio:"
+            label-for="input-fechaIni">
+            <b-form-datepicker
+              id="input-fechaIni"
+              v-model="filterFechaInicio"
+              placeholder="Fecha de inicio"
+              @change="handleFechaInicioChange"
+            ></b-form-datepicker>
+          </b-form-group>
+         
+        </b-col>
+        <b-col class="col-6 p-1 h-100">
+          <b-form-group
+            id="input-group-fechaFin"
+            label="Seleccione una fecha final:"
+            label-for="input-fechaFin">
+            <b-form-datepicker
+              id="input-fechaFin"
+              v-model="filterFechaFin"
+              placeholder="Fecha fin"
+              @change="handleFechaFinChange"
+            ></b-form-datepicker>
+          </b-form-group>
+        </b-col>
+    
+      </b-row> 
+      <b-row class="px-5 pb-5" >
+        <b-form-checkbox
+            id="checkbox-1"
+            v-model="status"
+            name="checkbox-1"
+            value="accepted"
+            unchecked-value="not_accepted"
+            @change="handleStatusChange"
+          >
+            Aplicar filtros
+          </b-form-checkbox>
+      </b-row>
+
+    
+
+       <b-row class="px-5">
+        <div ref="googleMap" class="google-map"></div>
+       </b-row>
+     
     </b-container>
   </div>
 </template>
@@ -110,29 +202,46 @@ export default {
       const map = new google.maps.Map(this.$refs.googleMap, {
         ...this.mapOptions,
       });
-
+      const infoWindow = new google.maps.InfoWindow();
       const googleMarkers = markers.map((marker) => {
-        const { denLat, denLng, denTdTitulo, denTitulo, denTdTipo } = marker;
+        const { denLat, denLng, denTdTitulo, denTitulo, denTdTipo ,denFecha, denEstTitulo} = marker;
         const position = new google.maps.LatLng(
           parseFloat(denLat),
           parseFloat(denLng)
         );
         const markerIcons = {
-          1: 'https://maps.google.com/mapfiles/ms/icons/red-dot.png',
-          2: 'https://maps.google.com/mapfiles/ms/icons/green-dot.png',
-          3: 'https://maps.google.com/mapfiles/ms/icons/blue-dot.png',
-          4: 'https://maps.google.com/mapfiles/ms/icons/purple-dot.png',
+          1: 'https://maps.google.com/mapfiles/ms/icons/green-dot.png',
+          2: 'https://maps.google.com/mapfiles/ms/icons/purple-dot.png',
+          3: 'https://maps.google.com/mapfiles/ms/icons/red-dot.png',
+          4: 'https://maps.google.com/mapfiles/ms/icons/blue-dot.png',
         };
 
         const icon = markerIcons[denTdTipo] || null;
 
-        return new google.maps.Marker({
+        const googleMarker = new google.maps.Marker({
           position,
           map,
           label: denTdTitulo,
           title: denTitulo,
-          icon: icon
+          icon: icon,
         });
+
+        // Agregar evento click al marcador para mostrar el infoWindow
+        googleMarker.addListener("click", () => {
+          const content = `
+            <div>
+              <h5>${denTdTitulo}</h5>
+              <p>${denTitulo}</p>
+              <p>Estado: ${denEstTitulo}</p>
+              <p>Fecha: ${denFecha}</p>
+            </div>
+          `;
+          infoWindow.setContent(content);
+          infoWindow.open(map, googleMarker);
+        });
+
+        return googleMarker;
+
       });
 
       new MarkerClusterer(map, googleMarkers, { imagePath: imgClusterUrl });
@@ -186,7 +295,9 @@ export default {
             denLng: denuncia.denLng,
             denTdTitulo: denuncia.denTdTitulo,
             denTitulo: denuncia.denTitulo,
-            denTdTipo: denuncia.denTipo
+            denTdTipo: denuncia.denTipo,
+            denEstTitulo: denuncia.denEstTitulo,
+            denFecha: denuncia.denFecha
           }));
           this.initMap();
           
