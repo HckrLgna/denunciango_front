fd5d78e355eea317d54a0041ab4e67acfb408274
Angel Eugenio Charca Blanco <ablanco837@gmail.com>
2023-07-03 22:58:16 -0400

    agregamos un input en la fila de la denuncia

--- src/components/ImboxTable.vue
+++ src/components/ImboxTable.vue
@@ -14,8 +14,13 @@
                 ></b-pagination>
             </b-col>
             <b-col col md="3" >
-                <b-dropdown id="dropdown-form" text="Dropdown with form" ref="dropdown" class="m-2">
+                <b-dropdown id="dropdown-form" text="Filtros"  block ref="dropdown" class="m-2" onclick="event.stopPropagation()">
                     <b-dropdown-form>
+                        <b-form-group label="Ordenar" v-slot="{ ariaDescribedby }">
+                            <b-form-radio v-model="selected" :aria-describedby="ariaDescribedby" name="some-radios" value="A">Ascendente</b-form-radio>
+                            <b-form-radio v-model="selected" :aria-describedby="ariaDescribedby" name="some-radios" value="B">Descendente</b-form-radio>
+                        </b-form-group>
+
                         <b-form-group
                             id="input-group-estado"
                             label="Seleccione un estado:"
@@ -42,12 +47,14 @@
                             ></b-form-select>
                         </b-form-group>
                         <b-dropdown-divider></b-dropdown-divider>
-                        <b-form-group
-                            id="input-group-fechaIni"
-                            label="Seleccione una fecha inicio:"
-                            label-for="input-fechaIni">
-                            <b-form-datepicker id="example-datepicker" v-model="value" class="mb-2"></b-form-datepicker>
-                        </b-form-group>
+
+                        <b-dropdown-item class="dropdownItem" style="min-width: 318px;">
+                            <b-form-datepicker id="customDatePicker" v-model="date"></b-form-datepicker>
+                            <div id="customCalendar" v-if="!isHidden">
+                            <b-calendar v-model="date"></b-calendar>
+                            </div>
+                        </b-dropdown-item>
+
                         <b-form-group
                             id="input-group-fechaFin"
                             label="Seleccione una fecha final:"
@@ -71,10 +78,15 @@
             <b-table
                 id="my-table"
                 :items="items"
+                :fields="fields"
                 :per-page="perPage"
                 :current-page="currentPage"
                 medium
-            ></b-table>
+            >
+            <template v-slot:cell(selected)="row">
+                 <input type="checkbox" v-model="row.item.selected">
+            </template>
+            </b-table>
             </div>
         </b-row>
     </b-container>
@@ -88,9 +100,18 @@
           perPage: 3,
           currentPage: 1,
           items: [
-            { descripcion: 'El parque del km 6 esta descuidado', fecha: '23/06/2023-11:59', categoria: 'Parques y jardines', estado: 'nueva', editar: 'editar', ver: 'mapa'},
+            {selected: true , descripcion: 'El parque del km 6 esta descuidado', fecha: '23/06/2023-11:59', categoria: 'Parques y jardines', estado: 'nueva', editar: 'editar', ver: 'mapa'},
            
           ],
+          fields: [
+                { key: 'selected', label: 'Seleccionar'  },
+                { key: 'descripcion', label: 'Descripcion' },
+                { key: 'fecha', label: 'Fecha' },
+                { key: 'categoria', label: 'Categoria' },
+                { key: 'estado', label: 'Estado' },
+                { key: 'editar', label: 'Editar' },
+                { key: 'ver', label: 'Ver' },
+            ],
           value: '',
           filterFechaInicio: null, // Fecha de inicio seleccionada para filtrar
           filterFechaFin: null,
@@ -98,12 +119,85 @@
           tipo: null, 
           tipos: [],
           estados: [],
+          date: null,
+          isHidden: true,
+          selected: ''
         }
+      },
+      mounted() {
+            let dropdown = this.$refs.dropdown;
+
+            const customDatePicker = document.getElementById('customDatePicker');
+            customDatePicker?.addEventListener('click', (event) => {
+                event.stopPropagation();
+                this.isHidden = !this.isHidden;
+                console.log(this.isHidden);
+            });
+
+            document.addEventListener("click", () => {
+            this.isHidden = true;
+            });
+
+            const customCalendar = document.getElementById('customCalendar');
+            customCalendar?.addEventListener('click',(event)=>{
+                event.stopPropagation();
+            })
+         
+        },
+      methods: {
+        handleEstadoChange() {
+            this.fetchMarkers();
+            },
+            // Método para manejar el evento de cambio de valor del selector de tipo
+            handleTipoChange() {
+            this.fetchMarkers();
+            },
+
+            // Método para manejar el evento de selección de fecha de inicio
+            handleFechaInicioChange() {
+            this.fetchMarkers();
+            },
+
+            // Método para manejar el evento de selección de fecha de fin
+            handleFechaFinChange() {
+            this.fetchMarkers();
+            },
+            handleStatusChange(){
+            console.log(this.status == "accepted");
+            if(this.status != "accepted"){
+                this.limpiarDatos();
+            }
+            this.fetchMarkers();
+            
+            },
+            limpiarDatos(){
+            console.log("entro limpiar datos")
+            this.status=false;
+            this.tipo=null;
+            this.estado=null;
+            this.filterFechaFin=null;
+            this.filterFechaInicio=null;
+            } 
       },
       computed: {
         rows() {
-          return this.items.length
-        }
-      }
+                return this.items.length
+                }
+      },
+
     }
   </script>
+  <style>
+    #customDatePicker {
+        width: 0;
+        height: 0;
+        padding: 0;
+    }
+    #customDatePicker > svg {
+        padding-left: 10px;
+        padding-right: 25px;
+    }
+    #customDatePicker__value_ {
+        margin-left: 30px;
+    }
+  </style>
